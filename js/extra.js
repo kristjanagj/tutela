193a1288af1abc6c598b5a663165bb5fb79a8e75
kristjanagj <44905351+kristjanagj@users.noreply.github.com>
2019-01-30 13:00:23 +0100

    changes

--- js/extra.js
+++ js/extra.js
@@ -2,44 +2,19 @@ $('.carousel').carousel({
     interval: 1500
 })
 
-// $('.loop').owlCarousel({
-//     center: true,
-//     items:2,
-//     loop:true,
-//     margin:10,
-//     responsive:{
-//         1600:{
-//             items:4
-//         }
-//     }
-// });
-
-// $('.owl-carousel').owlCarousel({
-//     margin:10,
-//     loop:true,
-//     autoWidth:true,
-//     items:4
-// })
-
-
-// $(document).ready(function(){
-//     $('.owl-carousel').owlCarousel();
-// });
-
-
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:5
-//         }
-//     }
-// })
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
